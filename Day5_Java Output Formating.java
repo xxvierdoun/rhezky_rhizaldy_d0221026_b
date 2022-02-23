3e44da61b82e2a3d420f48e35837a85b20986b3d
xxvierdoun <99780977+xxvierdoun@users.noreply.github.com>
2022-02-23 22:38:03 +0800

    Update Day5_Java Output Formating.java

--- Day5_Java Output Formating.java
+++ Day5_Java Output Formating.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class java output formating{
+public class java_output_formating{
 
     public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
