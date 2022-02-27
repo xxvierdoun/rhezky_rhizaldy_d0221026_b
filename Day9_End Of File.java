f0d1cf24fc39f265c5febcde7f0949d8aa781761
xxvierdoun <99780977+xxvierdoun@users.noreply.github.com>
2022-02-27 21:29:51 +0800

    Algoritma Dan Stuktur Data

---
+++ Day9_End Of File.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = 1;
+        while(sc.hasNext()){
+            String s = sc.nextLine();
+            System.out.println(r+" "+s);
+            r++;
+        }
+    }
+}
