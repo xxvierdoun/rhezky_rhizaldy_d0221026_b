6b703cfb8cd6be619d122ac75b1dc1ad4890d518
xxvierdoun <99780977+xxvierdoun@users.noreply.github.com>
2022-04-23 20:58:57 +0800

    Update Day63_Java MD V.java

--- Day63_Java MD V.java
+++ Day63_Java MD V.java
@@ -4,18 +4,20 @@ import java.security.*;
 
 public class Solution {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        /*
-         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
-         * class should be named Solution.
-         */
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner input = new Scanner(System.in);
-        MessageDigest m = MessageDigest.getInstance("SHA-256");
-        m.reset();
-        m.update(input.nextLine().getBytes());
-        for (byte i : m.digest()) {
-            System.out.print(String.format("%02x", i));
+        String str = input.next();
+        input.close();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(str.getBytes());
+            byte[] digest = md.digest();
+            for (byte byt : digest){
+                System.out.printf("%02x",byt);
+            }
+        } catch (Exception ex){
+            throw new RuntimeException(ex);
         }
-        System.out.println();
     }
 }
